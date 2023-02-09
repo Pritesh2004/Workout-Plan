5c73c8d5d2b16b91d49882da0f36b92a97c0155b
Pritesh2004 <pritesh21it@gmail.com>
2023-02-10 01:12:34 +0530

    first commit

--- app/src/main/java/com/example/workout_plan/SplashScreen.java
+++ app/src/main/java/com/example/workout_plan/SplashScreen.java
@@ -2,13 +2,30 @@ package com.example.workout_plan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class SplashScreen extends AppCompatActivity {
+Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },5000);
     }
 }
