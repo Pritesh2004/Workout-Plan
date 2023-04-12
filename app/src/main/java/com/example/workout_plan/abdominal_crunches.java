456160aa9b740d9f25cfb4554c2608847cd81c67
Patil Pritesh <pmpatilpritesh123@gmail.com>
2023-04-12 20:50:25 +0530

    report commit

--- app/src/main/java/com/example/workout_plan/abdominal_crunches.java
+++ app/src/main/java/com/example/workout_plan/abdominal_crunches.java
@@ -1,25 +1,26 @@
 package com.example.workout_plan;
-import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class jumping_jacks extends AppCompatActivity {
+public class abdominal_crunches extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jumping_jacks);
+        setContentView(R.layout.activity_abdominal_crunches);
         getSupportActionBar().hide();
 
 
     }
 
 
-    public void abdominal_crunches_page(View v){
-        Intent intent=new Intent(jumping_jacks.this,abdominal_crunches.class);
+    public void jumping_jacks(View v){
+
+        Intent intent=new Intent(abdominal_crunches.this,jumping_jacks.class);
         startActivity(intent);
     }
 }
